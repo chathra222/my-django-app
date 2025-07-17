ec3aedfcc032f9be8d46bbd091d933bd090671b2
Chathra Serasinghe <chathra@Chathras-MacBook-Pro.local>
2025-07-17 21:58:15 +0800

    testing again

--- users/views.py
+++ users/views.py
@@ -9,7 +9,6 @@ def add_user(request):
         try:
             data = json.loads(request.body)
             username = data.get("username")
-
             if not username:
                 return JsonResponse({"error": "Missing username"}, status=400)
             email = data.get("email")
